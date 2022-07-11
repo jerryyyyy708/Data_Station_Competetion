5b52065defedfe0f6b856747a3772fda22c3310e
jerryyyyy708 <84170074+jerryyyyy708@users.noreply.github.com>
2022-07-11 21:43:22 +0800

    Create transform_data_v1.py

---
+++ preprocess/transform_data_v1.py
@@ -0,0 +1,41 @@
+#V1: only use product counts
+import pandas as pd
+
+def create_dataframe(products):
+    df_head=[str(i)+'_count' for i in range(products)]
+    df_tail=[str(i)+'_buy' for i in range(products)]
+    df_head.append('customer')
+    df_head.append('age_group')
+    df_head+=df_tail
+    df=pd.DataFrame(columns=df_head)
+    return df
+
+df=pd.read_csv('encoded.csv')
+df=df.drop(columns=['Unnamed: 0'])
+
+customers= df['customer'].unique().tolist()
+products = df['product'].unique().tolist()
+new_dataframe=create_dataframe(len(products))
+
+for i in customers:
+    customer_df = df.loc[df.customer == i]
+    values = customer_df['product'].value_counts().keys().tolist()
+    counts = customer_df['product'].value_counts().tolist()
+    orders = customer_df['id'].unique().tolist()
+
+    tempdf = customer_df.loc[customer_df.id != orders[-1]]
+    target = customer_df.loc[customer_df.id == orders[-1]]
+
+    target_orders = target['product'].unique().tolist()
+
+    new_row = [0] * len(products)
+    new_tail = [0] * len(products)
+    new_row += [i,tempdf.iloc[0]['age_group']]
+    new_row += new_tail
+    for j , k in zip(values, counts):
+        new_row[int(j)] = k
+    for j in target_orders:
+        new_row[len(products)+2+int(j)] = 1 #if add column 2 need to change
+    new_dataframe.loc[len(new_dataframe)] = new_row
+
+new_dataframe.to_csv("preprocessed_v1.csv")
